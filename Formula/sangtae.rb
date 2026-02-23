4867da1c42fb0485b4941ffa2b0760321eab0835
JK,Kim <d0nzs00n@gmail.com>
2026-02-23 14:32:45 +0900

    Initial commit: Add Sangtae cask

---
+++ Formula/sangtae.rb
@@ -0,0 +1,16 @@
+cask "sangtae" do
+  version "1.0.1"
+  sha256 "76a11d8cadee53616c2d4b6ca15bc5a5a8692a7a0b1260bd6b3478a7b0357d37"
+
+  url "https://github.com/smok95/Sangtae/releases/download/v#{version}/Sangtae.dmg"
+  name "Sangtae"
+  desc "System monitor for macOS"
+  homepage "https://github.com/smok95/Sangtae"
+
+  app "Sangtae.app"
+
+  zap trash: [
+    "~/Library/Caches/com.gemini.Sangtae",
+    "~/Library/Preferences/com.gemini.Sangtae.plist",
+  ]
+end
